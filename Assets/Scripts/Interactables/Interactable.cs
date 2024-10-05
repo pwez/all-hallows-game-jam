9e4cee11015bfad25b40c82dec45b2405eab2d25
Robert Kuropkat <robert@kuropkat.com>
2024-10-05 12:21:26 -0400

    Add Blender Clas

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -1,41 +1,12 @@
 using System;
-using Players;
-using Players.Controllers;
-using UnityEngine;
 
-namespace Interactables {
+namespace Interactables
+{
 
     // The following is an example of how we can implement a general interactable mechanic
-    
+
     public interface IInteractable {
         void OnInteract();
-    }
-    
-    public class Door : IInteractable {
-        public void OnInteract() {
-            throw new NotImplementedException();
-        }
-    }
-    
-    public class InteractionTrigger : MonoBehaviour {
-
-        private IInteractable _interactable;
-        
-        private void OnTriggerEnter(Collider other) {
-            if (!IsPlayer(other, out var player)) return;
-            if (!IsPressingInteractButton(player)) return;
-            
-            _interactable.OnInteract();
-        }
-
-        private bool IsPressingInteractButton(IPlayer player) {
-            return player.Controller.Get<IActionInput>().IsPressed;
-        }
-
-        private bool IsPlayer(Collider other, out IPlayer player) {
-            return other.TryGetComponent<IPlayer>(out player);
-        }
-        
     }
     
 }
---
+++ Assets/Scripts/Interactables/InteractionTrigger.cs
@@ -0,0 +1,28 @@
+﻿using Players;
+using Players.Controllers;
+using UnityEngine;
+
+namespace Interactables
+{
+    public class InteractionTrigger : MonoBehaviour {
+
+        private IInteractable _interactable;
+        
+        private void OnTriggerEnter(Collider other) {
+            if (!IsPlayer(other, out var player)) return;
+            if (!IsPressingInteractButton(player)) return;
+            
+            _interactable.OnInteract();
+        }
+
+        private bool IsPressingInteractButton(IPlayer player) {
+            return player.Controller.Get<IActionInput>().IsPressed;
+        }
+
+        private bool IsPlayer(Collider other, out IPlayer player) {
+            return other.TryGetComponent<IPlayer>(out player);
+        }
+        
+    }
+    
+}

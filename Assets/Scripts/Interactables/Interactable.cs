b9fb7124a0e92c88220e2ac54633a45e891b86ca
Robert Kuropkat <robert@kuropkat.com>
2024-10-05 12:29:52 -0400

    Resolve merge conflict on Interactable

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -9,35 +9,4 @@ namespace Interactables
         void OnInteract();
     }
     
-<<<<<<< HEAD
-=======
-    public class Door : IInteractable {
-        public void OnInteract() {
-            throw new NotImplementedException();
-        }
-    }
-    
-    public class InteractionTrigger : MonoBehaviour {
-
-        private IInteractable _interactable;
-        
-        private void OnTriggerEnter(Collider other) {
-            if (!IsPlayer(other, out var player)) return;
-            if (!IsPressingInteractButton(player)) return;
-            
-            _interactable.OnInteract();
-        }
-
-        private bool IsPressingInteractButton(IPlayer player)
-        {
-            return player.Controller.ActionInput.IsPressed;
-        }
-
-        private bool IsPlayer(Collider other, out IPlayer player) {
-            return other.TryGetComponent<IPlayer>(out player);
-        }
-        
-    }
-    
->>>>>>> 9ef3f62fa8520e0174aadd73b47740bf46bdb175
 }

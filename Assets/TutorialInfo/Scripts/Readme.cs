b96c53c7d8d44495b7e3b8e1f59e44ca9753569c
Robert Kuropkat <robert@kuropkat.com>
2024-10-05 14:23:30 -0400

    merge conflcits

--- Assets/TutorialInfo/Scripts/Readme.cs
+++ Assets/TutorialInfo/Scripts/Readme.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using UnityEngine;
 
-<<<<<<< HEAD
 public class Readme : ScriptableObject {
 	public Texture2D icon;
 	public float iconMaxWidth = 128f;
@@ -14,21 +13,4 @@ public class Readme : ScriptableObject {
 	public class Section {
 		public string heading, text, linkText, url;
 	}
-=======
-namespace TutorialInfo.Scripts
-{
-    public class Readme : ScriptableObject
-    {
-        public Texture2D icon;
-        public string title;
-        public Section[] sections;
-        public bool loadedLayout;
-
-        [Serializable]
-        public class Section
-        {
-            public string heading, text, linkText, url;
-        }
-    }
->>>>>>> b9fb7124a0e92c88220e2ac54633a45e891b86ca
 }

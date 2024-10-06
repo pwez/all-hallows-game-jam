a560f55cceca76d616b4b6252a3c592cc1abc401
eytantal <developer.eytan.tal@gmail.com>
2024-10-06 13:54:28 -0400

    Updated goal trigger and prefab to play sound

--- Assets/Scripts/Game/GoalTrigger.cs
+++ Assets/Scripts/Game/GoalTrigger.cs
@@ -6,6 +6,17 @@ namespace Game {
 
         public GameManager gameManager;
         public bool wasTriggered;
+
+        [Header("Parameters")] 
+        public AudioClip audioClip;
+        public AudioSource audioSource;
+        public float audioPlayLength;
+        public GameObject componentToDestroy;
+        
+
+        private void Awake() {
+            audioSource.clip = audioClip;
+        }
         
         private void OnTriggerStay(Collider other) {
             if (wasTriggered) return;
@@ -15,6 +26,10 @@ namespace Game {
 
             var actionInputIsPressed = player.Controller.ActionInput.IsHeld;
             if (!actionInputIsPressed) return;
+            
+            audioSource.Play();
+            Destroy(componentToDestroy);
+            Destroy(audioSource, audioPlayLength);
             
             gameManager.OnGoalReached();
             wasTriggered = true;
